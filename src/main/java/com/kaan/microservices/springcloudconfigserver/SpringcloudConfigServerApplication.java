06afa6201618bbb6428b3600e04df236fbc37d8e
Kaan Bobac <bobackaan@gmail.com>
2021-06-25 14:43:54 +0300

    initial commit

---
+++ src/main/java/com/kaan/microservices/springcloudconfigserver/SpringcloudConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.kaan.microservices.springcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudConfigServerApplication.class, args);
+	}
+
+}
